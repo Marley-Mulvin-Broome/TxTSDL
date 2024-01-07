d189d40c505fe3d815ca3eedc42d12d1d2e426fb
Marley Mulvin Broome <marley.developer@gmail.com>
2024-01-07 16:28:46 +0900

    Made it so TUIInput only accepts alphabet characters

--- src/tui/tui_input.c
+++ src/tui/tui_input.c
@@ -70,7 +70,32 @@ void TuiInputKeyPress(TuiInput *input, int key) {
         case SDLK_BACKSPACE: // Backspace
             backspaceText(input);
             break;
-        default:
+        case SDLK_a:
+        case SDLK_b:
+        case SDLK_c:
+        case SDLK_d:
+        case SDLK_e:
+        case SDLK_f:
+        case SDLK_g:
+        case SDLK_h:
+        case SDLK_i:
+        case SDLK_j:
+        case SDLK_k:
+        case SDLK_l:
+        case SDLK_m:
+        case SDLK_n:
+        case SDLK_o:
+        case SDLK_p:
+        case SDLK_q:
+        case SDLK_r:
+        case SDLK_s:
+        case SDLK_t:
+        case SDLK_u:
+        case SDLK_v:
+        case SDLK_w:
+        case SDLK_x:
+        case SDLK_y:
+        case SDLK_z:
             inputCharacter(input, key);
             break;
     }
